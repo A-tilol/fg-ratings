b3c2eb9e5b66bb2c6218dbcfd006f4429695cb3d
atilol <ggrkssk@gmail.com>
2023-07-23 00:20:50 +0900

    -

--- ui/build.sh
+++ ui/build.sh
@@ -2,6 +2,14 @@
 
 set -xeu
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "gh-pages" ]; then
+  echo "現在のブランチは gh-pages ではありません。buildはgh-pagesで行ってください"
+  exit 1
+fi
+
 ng build --output-path ../docs
 
 cp ../docs/index.html ../docs/404.html
+
+git add ..
