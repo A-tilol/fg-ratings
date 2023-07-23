77f14c9ba0a9b8ed51348347e604927cd1991af0
atilol <ggrkssk@gmail.com>
2023-07-23 16:57:10 +0900

    -

---
+++ ui/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -xeu
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "gh-pages" ]; then
+    no_commited_files=$(git status --porcelain)
+    if [ -n "$no_commited_files" ]; then
+        echo "コミットされていない変更があります。"
+        echo "$no_commited_files"
+        exit 1
+    fi
+    git checkout gh-pages
+fi
+
+git merge main
+
+ng build --output-path ../docs
+
+cp ../docs/index.html ../docs/404.html
+
+git add ..
+git commit -m -
+
+git checkout main
