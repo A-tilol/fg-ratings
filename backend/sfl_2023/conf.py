0eb3c06970b94b4a48683ba3c54053beb181e49d
atilol <ggrkssk@gmail.com>
2023-08-20 16:19:14 +0900

    -

---
+++ backend/sfl_2023/conf.py
@@ -0,0 +1,11 @@
+K = 32  # SFLだけだと試合数が少ないため大きめを設定
+INITIAL_RATING = 1500
+
+PLAYER_TSV_PATH = "./data/sfl_2023/players.tsv"
+RESULTS_TSV_PATH = "./data/sfl_2023/results.tsv"
+TEAM_RESULTS_TSV_PATH = "./data/sfl_2023/team_results.tsv"
+RATINGS_TSV_PATH = "./data/sfl_2023/ratings.tsv"
+TEAM_RATINGS_TSV_PATH = "./data/sfl_2023/team_ratings.tsv"
+PLAYER_DATA_TSV_PATH = "./data/sfl_2023/player_data.tsv"
+
+SFL_2023_RESULTS_DIR = "./data/sfl_2023/results"
